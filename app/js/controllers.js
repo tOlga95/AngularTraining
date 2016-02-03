1f3000369d459e76740e55e8d1285d87880d3066
Olga Trofimova <steshic@mail.ru>
2016-02-03 12:37:02 +0200

    Lesson 1 - Declare underscore as a dependency and use it in controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,5 +1,14 @@
-function MyCtrl($scope) {
+var underscore = angular.module('underscore', [])
+underscore.factory('_', ['$window', function($window) {
+  return $window._
+}])
+	
+var app = angular.module('myApp', ['underscore'])
+	
+app.controller('MyCtrl', function($scope, _) {
     $scope.sum = function () {
-        return parseInt($scope.first) + parseInt($scope.second);
-    };
-}
+	var numbers = _.values(_.pick($scope, 'first', 'second'))
+		if(_.isEmpty(numbers)) return 0
+        return _.reduce(numbers, function(memo, num){ return memo + parseInt(num) }, 0)
+    }
+})
