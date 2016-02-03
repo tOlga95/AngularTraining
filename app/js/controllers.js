d029936f56a10707f003714e00e2678bf01d88df
Olga Trofimova <steshic@mail.ru>
2016-02-03 13:06:58 +0200

    Lesson 1 - Remove redundant wrapper

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,10 +5,10 @@ underscore.factory('_', ['$window', function($window) {
 	
 var app = angular.module('myApp', ['underscore'])
 	
-app.controller('MyCtrl', function($scope, _) {
-    $scope.sum = function () {
+app.controller('MyCtrl', function ($scope, _) {
+	$scope.sum = function () {
 		var numbers = _.values(_.pick($scope, 'first', 'second'))
-		if(_.isEmpty(numbers)) return 0
-        return _.reduce(numbers, function(memo, num){ return memo + parseInt(num) }, 0)
-    }
+		if (_.isEmpty(numbers)) return 0
+		return _.reduce(numbers, function (memo, num) { return memo + parseInt(num) }, 0)
+	}
 })
