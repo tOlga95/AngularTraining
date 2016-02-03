39e9958616ef0ea496d08653438cb5956bcd7997
Olga Trofimova <steshic@mail.ru>
2016-02-03 12:40:35 +0200

    Lesson 1 - Fix code formatting

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,7 +7,7 @@ var app = angular.module('myApp', ['underscore'])
 	
 app.controller('MyCtrl', function($scope, _) {
     $scope.sum = function () {
-	var numbers = _.values(_.pick($scope, 'first', 'second'))
+		var numbers = _.values(_.pick($scope, 'first', 'second'))
 		if(_.isEmpty(numbers)) return 0
         return _.reduce(numbers, function(memo, num){ return memo + parseInt(num) }, 0)
     }
