b9e82a2cd2598d0d56f84b33fdcbcb9865bb4a64
tOlga <steshicoa@mail.com>
2016-02-08 00:41:42 +0300

    Lesson 2: Add custom directive

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,10 +5,17 @@ underscore.factory('_', ['$window', function($window) {
 	
 var app = angular.module('myApp', ['underscore'])
 	
-app.controller('MyCtrl', function ($scope, _) {
-	$scope.sum = function () {
-		var numbers = _.values(_.pick($scope, 'first', 'second'))
-		if (_.isEmpty(numbers)) return 0
-		return _.reduce(numbers, function (memo, num) { return memo + parseInt(num) }, 0)
-	}
-})
+app
+	.controller('MyCtrl', function ($scope, _) {
+		$scope.sum = function () {
+			var numbers = _.values(_.pick($scope, 'first', 'second'))
+			if (_.isEmpty(numbers)) return 0
+			return _.reduce(numbers, function (memo, num) { return memo + parseInt(num) }, 0)
+		}
+	})
+	.directive('numbersSum', function() {
+		return {
+			restrict: 'E',
+			templateUrl: "templates/Sum.html"
+		}
+	})
